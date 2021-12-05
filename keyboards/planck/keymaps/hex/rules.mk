86854747374047daed25beb1ab7adbed4cb3bb70
Hexadecimal Hyuga <hexadecimaaal@gmail.com>
2021-12-05 17:45:15 +0800

    hexboard patches

---
+++ keyboards/planck/keymaps/hex/rules.mk
@@ -0,0 +1,12 @@
+# https://beta.docs.qmk.fm/reference/config-options#feature-options
+# Features Specifically Wanted
+EXTRAKEY_ENABLE = yes
+STENO_ENABLE = yes
+NKRO_ENABLE = yes
+AUDIO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+MIDI_ENABLE = no
+
+# Features taking up space
+CONSOLE_ENABLE = no
+
