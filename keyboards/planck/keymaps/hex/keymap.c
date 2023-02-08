d96f77d1fdef702479bb37a984844d90daef4ddb
Hexadecimal <hexadecimaaal@gmail.com>
2023-02-09 02:16:08 +0800

    update planck + rebase

--- keyboards/planck/keymaps/hex/keymap.c
+++ keyboards/planck/keymaps/hex/keymap.c
@@ -38,49 +38,50 @@ enum planck_keycodes {
 
 #define FUNC MO(_FUNC)
 #define NUM MO(_NUM)
+#define C_T_ESC LCTL_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT_planck_grid(
    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-  KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
-  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,   KC_UP, XXXXXXX,
-   KC_ESC, XXXXXXX, KC_LGUI, KC_LALT,    FUNC,  KC_SPC,  KC_SPC,     NUM, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+  C_T_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,
+  KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+   KC_ESC, XXXXXXX, KC_LGUI, KC_LALT,    FUNC,  KC_SPC,  KC_SPC,     NUM, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
 ),
 
 [_NUM] = LAYOUT_planck_grid(
-  _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_MINS,  KC_EQL, KC_QUOT,
-  _______, KC_INT1, KC_INT2, KC_INT3, KC_INT4, KC_INT5, KC_NUBS, KC_NUHS, _______, KC_SLSH, KC_PGUP, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN,  KC_END
+   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSLS,
+  _______, KC_INT1, KC_INT2, KC_INT3, KC_INT4, KC_INT5, _______, KC_LBRC, KC_RBRC, KC_MINS,  KC_EQL, KC_QUOT,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NUBS, KC_NUHS, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 [_FUNC] = LAYOUT_planck_grid(
   KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_PSCR,
   _______,  KC_INS, KC_MUTE, KC_VOLD, KC_VOLU,  KC_BRK, KC_WH_L, KC_BTN3, KC_WH_U,  KC_F11,  KC_F12, _______,
-  _______,  KC_APP, KC_MPLY, KC_MPRV, KC_MNXT,  KC_APP, KC_WH_R, KC_BTN2, KC_WH_D, KC_BTN1, KC_MS_U, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
+  _______,  KC_APP, KC_MPLY, KC_MPRV, KC_MNXT,  KC_APP, KC_WH_R, _______, KC_WH_D, KC_BTN1, KC_BTN2, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
 ),
 
 [_ADJUST] = LAYOUT_planck_grid(
-  XXXXXXX, RESET,   DEBUG,   XXXXXXX, MIDI,    AU_ON,   AU_OFF,  RGB_HUI, RGB_HUD, RGB_MOD,RGB_RMOD, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MUV_IN,  MUV_DE,  RGB_SAI, RGB_SAD, RGB_SPI, RGB_SPD, XXXXXXX,
-  XXXXXXX, PLOVER,  DISCO,   XXXXXXX, MU_MOD,  MU_ON,   MU_OFF,  RGB_VAI, RGB_VAD, RGB_TOG, XXXXXXX, XXXXXXX,
+  XXXXXXX, QK_BOOT, DB_TOGG, XXXXXXX,    MIDI,   AU_ON,  AU_OFF, RGB_HUI, RGB_HUD, RGB_MOD,RGB_RMOD, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  MUV_IN,  MUV_DE, RGB_SAI, RGB_SAD, RGB_SPI, RGB_SPD, XXXXXXX,
+  XXXXXXX,   PLOVER,  DISCO, XXXXXXX,  MU_MOD,   MU_ON,  MU_OFF, RGB_VAI, RGB_VAD, RGB_TOG, XXXXXXX, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 [_PLOVER] = LAYOUT_planck_grid(
-  STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC,
-  STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
-  XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
-  EXT_PLV, XXXXXXX, XXXXXXX, STN_A,   STN_O,   XXXXXXX, XXXXXXX, STN_E,   STN_U,   STN_PWR, STN_RE1, STN_RE2
+   STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC,
+   STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL, STN_ST1, STN_ST3,  STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
+  XXXXXXX,  STN_S2,  STN_KL,  STN_WL,  STN_RL, STN_ST2, STN_ST4,  STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
+  EXT_PLV, XXXXXXX, XXXXXXX,   STN_A,   STN_O, XXXXXXX, XXXXXXX,   STN_E,   STN_U, STN_PWR, STN_RE1, STN_RE2
 ),
 
 [_MIDI] = LAYOUT_planck_grid(
-     MI_As,   MI_B,   MI_C_1,  MI_Cs_1,   MI_D_1,  MI_Ds_1,   MI_E_1,   MI_F_1, MI_Fs_1,  MI_G_1, MI_Gs_1,  MI_A_1,
-      MI_F,  MI_Fs,     MI_G,    MI_Gs,     MI_A,    MI_As,     MI_B,   MI_C_1, MI_Cs_1,  MI_D_1, MI_Ds_1,  MI_E_1,
-      MI_C,  MI_Cs,     MI_D,    MI_Ds,     MI_E,     MI_F,    MI_Fs,     MI_G,   MI_Gs,    MI_A,   MI_As,    MI_B,
-  EXT_MIDI, MI_MOD, MI_MODSD, MI_MODSU, MI_BENDD, MI_BENDU, MI_TRNSD, MI_TRNSU, MI_OCTD, MI_OCTU, MI_VELD, MI_VELU
+    MI_As,    MI_B,  MI_C_1, MI_Cs_1,  MI_D_1, MI_Ds_1,  MI_E_1,  MI_F_1, MI_Fs_1,  MI_G_1, MI_Gs_1,  MI_A_1,
+     MI_F,   MI_Fs,    MI_G,   MI_Gs,    MI_A,   MI_As,    MI_B,  MI_C_1, MI_Cs_1,  MI_D_1, MI_Ds_1,  MI_E_1,
+     MI_C,   MI_Cs,    MI_D,   MI_Ds,    MI_E,    MI_F,   MI_Fs,    MI_G,   MI_Gs,    MI_A,   MI_As,    MI_B,
+  EXT_MIDI, MI_MOD,MI_MODSD,MI_MODSU,MI_BENDD,MI_BENDU,MI_TRNSD,MI_TRNSU, MI_OCTD, MI_OCTU, MI_VELD, MI_VELU
 )
 
 };
